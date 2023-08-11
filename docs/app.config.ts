3ff21235bd5ba3187c3143e243042df04a7d825e
Farnabaz <farnabaz@gmail.com>
2023-08-11 16:37:00 +0330

    docs: setup & syntax

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,38 +1,46 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Remark MDC',
+    description: 'Remark plugin to parse MarkDown Components syntax.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+
     socials: {
       twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
+      github: 'nuxtlabs/remark-mdc',
       nuxt: {
         label: 'Nuxt',
         icon: 'simple-icons:nuxtdotjs',
         href: 'https://nuxt.com'
       }
     },
+
     github: {
-      dir: '.starters/default/content',
+      dir: 'docs/content',
       branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
+      repo: 'remark-mdc',
+      owner: 'nuxtlabs',
       edit: true
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
       padded: true,
       fluid: true
     },
+
     header: {
-      logo: true,
+      logo: false,
       showLinkIcon: true,
       exclude: [],
-      fluid: true
-    }
+      fluid: true,
+      title: 'Remark MDC'
+    },
+
+    titleTemplate: '%s · Remark MDC'
   }
 })
