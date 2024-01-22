d58f607a8afb146c7cece865929d551f115ce632
Farnabaz <farnabaz@gmail.com>
2024-01-22 16:42:57 +0100

    fix: do not flatted data in fm stringify (#70)
    All fm data should be written in standard YAML format

---
+++ test/basic.test.ts
@@ -0,0 +1,31 @@
+import { describe } from 'vitest'
+import { runMarkdownTests } from './utils'
+
+describe('basic', () => {
+  runMarkdownTests({
+    simple: {
+      mdcOptions: {
+        experimental: {
+          autoUnwrap: true
+        }
+      },
+      markdown: [
+        'paragraph 1',
+        '',
+        '---',
+        '',
+        'paragraph 2'
+      ].join('\n')
+    },
+    link: {
+      mdcOptions: {
+        experimental: {
+          autoUnwrap: true
+        }
+      },
+      markdown: [
+        '[link](https://nuxtjs.org){target="_blank"}'
+      ].join('\n')
+    }
+  })
+})
