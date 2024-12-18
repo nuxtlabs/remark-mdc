533ee0ccd51a1544c6198e3fefd90dc43a2c4a66
Farnabaz <farnabaz@gmail.com>
2024-12-18 14:36:47 +0100

    feat: expose micromark extension

--- src/index.ts
+++ src/index.ts
@@ -120,3 +120,4 @@ function bindData(data: Record<string, any>) {
 }
 
 export { stringifyFrontMatter, parseFrontMatter } from './frontmatter'
+export { default as micromarkExtension } from './micromark-extension'
