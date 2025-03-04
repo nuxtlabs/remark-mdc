2e78971f399ed626bca6201bf1c6e3337fbf3baf
Farnabaz <farnabaz@gmail.com>
2025-03-04 14:05:54 +0100

    fix(to-markdown): indent container contens

--- test/unwrap.test.ts
+++ test/unwrap.test.ts
@@ -67,6 +67,80 @@ describe('auto-unwrap', () => {
           autoUnwrap: true,
         },
       },
+      markdown: '::test\n:::another-component\n:::\n::',
+      extra(_markdown, ast, _expected) {
+        expect(ast.children[0].children.length).toBe(1)
+        expect(ast.children[0].children[0]).property('type', 'containerComponent')
+      },
+    },
+  }, { indentContent: false })
+
+  // Indented
+  runMarkdownTests({
+    simple: {
+      mdcOptions: {
+        experimental: {
+          autoUnwrap: true,
+        },
+      },
+      markdown: '::test\n  Hello World\n::',
+      extra(_markdown, ast, _expected) {
+        expect(ast.children[0].children.length).toBe(1)
+        expect(ast.children[0].children[0]).property('type', 'text')
+        expect(ast.children[0].children[0]).property('value', 'Hello World')
+      },
+    },
+    slot: {
+      mdcOptions: {
+        experimental: {
+          autoUnwrap: true,
+        },
+      },
+      markdown: '::test\n#title\n  Hello World\n::',
+      extra(_markdown, ast, _expected) {
+        const slot = ast.children[0].children[0]
+        expect(slot.children.length).toBe(1)
+        expect(slot.children[0]).property('type', 'text')
+        expect(slot.children[0]).property('value', 'Hello World')
+      },
+    },
+    twoSlots: {
+      mdcOptions: {
+        experimental: {
+          autoUnwrap: true,
+        },
+      },
+      markdown: '::test\n  Foo bar\n\n#title\n  Hello World\n::',
+      extra(_markdown, ast, _expected) {
+        const nonSlotChild = ast.children[0].children[0]
+        expect(nonSlotChild).property('type', 'text')
+        expect(nonSlotChild).property('value', 'Foo bar')
+
+        const slot = ast.children[0].children[1]
+        expect(slot.children.length).toBe(1)
+        expect(slot.children[0]).property('type', 'text')
+        expect(slot.children[0]).property('value', 'Hello World')
+      },
+    },
+    twoChildren: {
+      mdcOptions: {
+        experimental: {
+          autoUnwrap: true,
+        },
+      },
+      markdown: '::test\n  Foo bar\n\n  Hello World\n::',
+      extra(_markdown, ast, _expected) {
+        for (const child of ast.children[0].children) {
+          expect(child).property('type', 'paragraph')
+        }
+      },
+    },
+    componentChildren: {
+      mdcOptions: {
+        experimental: {
+          autoUnwrap: true,
+        },
+      },
       markdown: '::test\n  :::another-component\n  :::\n::',
       extra(_markdown, ast, _expected) {
         expect(ast.children[0].children.length).toBe(1)
